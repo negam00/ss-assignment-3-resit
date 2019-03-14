4267a293100e9fefd8470714f2fb5cdd125b62b5
negam00 <34008254+negam00@users.noreply.github.com>
2019-03-14 14:38:53 +0100

    test search

--- src/test/java/nl/hva/ict/ss/textsearch/BackwardsSearchTest.java
+++ src/test/java/nl/hva/ict/ss/textsearch/BackwardsSearchTest.java
@@ -1,75 +1,42 @@
 package nl.hva.ict.ss.textsearch;
-
-import org.junit.Before;
 import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
+import nl.hva.ict.ss.textsearch.BackwardsSearch;
+
 
 public class BackwardsSearchTest {
-    protected BackwardsSearch searchEngine;
-
-    @Before
-    public void setup() {
-
-    }
 
     @Test
-    public void bigMiddleOfSearch() {
-        BackwardsSearch back = new BackwardsSearch("nes");
-
-
-        assertEquals("Thisstringcontainsalotlotlotoftextlikeaveryseriouslylargeamountoftextthesearchengi".length(), back.findLocation("nes","Thisstringcontainsalotlotlotoftextlikeaveryseriouslylargeamountoftextthesearchengineswillhavetoperformalotofcomparisonsinordertofindthewordthatishiddenwithinthistext"));
-    }
-
-    @Test
-    public void endOfSearch(){
-        BackwardsSearch back = new BackwardsSearch("word");
-
-        assertEquals("thistextcontainsa".length(),back.findLocation("word","thistextcontainsaword"));
-    }
-
-    @Test
-    public void beginOfSearch(){
-        BackwardsSearch back = new BackwardsSearch("this");
-
-        assertEquals("".length(),back.findLocation("this","thistextcontainsaword"));
-    }
-
-    @Test
-    public void middleOfSearch(){
-        BackwardsSearch back = new BackwardsSearch("b");
-
-        assertEquals("aaaa".length(),back.findLocation("b","aaaabaaaa"));
-    }
-
-
-
-    @Test
-    public void search() {
-        BackwardsSearch back = new BackwardsSearch("needle");
-
-        assertEquals("whatisinhereohitsa".length(),back.findLocation("needle","whatisinhereohitsaneedleow"));
-
+    public void searchPattern(){
+        BackwardsSearch backwardsSearch = new BackwardsSearch("potato");
+        assertEquals("potototototota".length(), nl.hva.ict.ss.textsearch.backwardsSearch.search("pototototototapotatotototo"));
     }
 
 
     @Test
-    public void findSingleOccurrence() {
-        BackwardsSearch back = new BackwardsSearch("needle");
-
-
-        assertEquals("whereisthe".length(), back.findLocation("needle","whereistheneedleinthishaystack"));
+    public void searchEndOfWordPattern(){
+        BackwardsSearch backwardsSearch = new BackwardsSearch("pattern");
+        assertEquals("whereisthehidden".length(),nl.hva.ict.ss.textsearch.backwardsSearch.search("whereisthehiddenpattern"));
     }
-    //needle
-    //"whereistheneedleinthishaystack");
+
+
     @Test
-    public void cantFindOccurrence() {
-        BackwardsSearch back = new BackwardsSearch("needle");
-
-        int index = back.findLocation("needle", "thereisnothinginthishaystack");
-
-        assertEquals(-1, index);
+    public void searchMiddleOfWordPattern(){
+        BackwardsSearch backwardsSearch = new BackwardsSearch("pattern");
+        assertEquals("whereistn".length(),nl.hva.ict.ss.textsearch.backwardsSearch.search("whereistnpatternhehidden"));
     }
 
+
+    @Test
+    public void searchStartOfWordPattern(){
+        BackwardsSearch backwardsSearch = new BackwardsSearch("pattern");
+        assertEquals("whereistn".length(),nl.hva.ict.ss.textsearch.backwardsSearch.search("whereistnpatternhehidden"));
+    }
+
+    @Test
+    public void searchHugeWordPattern(){
+        BackwardsSearch backwardsSearch = new BackwardsSearch("ditiseenlangpattern");
+        assertEquals("ditiseenlangditiseenlangpatditiseenlangpatditpatternlangpatternditpatterniseenpatter".length(),backwardsSearch.search("ditiseenlangditiseenlangpatditiseenlangpatditpatternlangpatternditpatterniseenpatterditiseenlangpatternditisditiseenlangditiseenlpattern"));
+    }
 
 }
